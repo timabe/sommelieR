ae30c89dfcb4a955e13c5513825d53cf4b4f835d
Tim Abraham <tabraham@twitter.com>
2014-03-17 21:46:13 -0700

    changed some copy in ui and links open in new tabs

--- ui.R
+++ ui.R
@@ -5,14 +5,14 @@ shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel(list(tags$head(tags$style(".span12")),
-                   "Wine Recommender", 
+                   "Wine Sommelier", 
               HTML("<p style='float:right;
                               font-size:14px;
                               font-weight:normal;
                               text-align:right;
                               line-height:normal;'>
-                    <a href='http://www.github.com/timabe/sommelieR'> source code <br>
-                    <a href='http://picklesanddata.com/data/?p=161' > how it works </a> <br></p>"))
+                    <a href='http://www.github.com/timabe/sommelieR' target='_blank'> source code <br>
+                    <a href='http://picklesanddata.com/data/?p=161' target='_blank'> how it works </a> <br></p>"))
                ),
 
 
@@ -24,7 +24,7 @@ shinyUI(pageWithSidebar(
     ),
     
     
-     textInput('w', label = 'Describe what you want in a wine and your recommendation will appear!', value = ""),          
+     textInput('w', label = 'Describe your ideal wine and the chart updates with the best varietals!', value = ""),          
   
     actionButton('button', "I need help thinking of descriptive words")
   ),
